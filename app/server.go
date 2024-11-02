da30ccb3c7834f87ace62fd83a4f1b8d9c3c9e6b
khostya <khostya.konstantin@gmail.com>
2024-11-02 06:53:24 +0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -37,11 +39,75 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 
-		if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
-			fmt.Println("Error write +PONG: ", err.Error())
-		}
+		println(string(buf[:n]))
+
+		arr, _, err := decode(buf)
 		if err != nil {
-			return
+			conn.Write([]byte(fmt.Errorf("error decode command: %w", err).Error()))
+			continue
+		}
+
+		if arr[0] == "PING" {
+			_, _ = conn.Write(encodeString("PONG"))
+		} else if arr[0] == "ECHO" {
+			_, _ = conn.Write(encodeString(arr[1]))
 		}
 	}
+}
+
+func decode(buf []byte) ([]string, []byte, error) {
+	s := string(buf[0])
+	buf = buf[1:]
+
+	if s == "*" {
+		return array(buf)
+	}
+	return nil, nil, errors.New("Unknown command: " + s)
+}
+
+func array(buf []byte) ([]string, []byte, error) {
+	s := string(buf)
+	println(s)
+
+	arr := make([]string, 0)
+
+	n, err := strconv.Atoi(string(buf[0]))
+	if err != nil {
+		return nil, nil, err
+	}
+	buf = buf[3:]
+	println(arr, n)
+
+	for i := 0; i < n; i++ {
+		s := string(buf)
+		println(s)
+
+		ch := string(buf[0])
+		if ch == "$" {
+			s, b, err := str(buf)
+			buf = b
+
+			if err != nil {
+				return nil, nil, err
+			}
+			arr = append(arr, s)
+		}
+	}
+
+	return arr, buf, nil
+}
+
+func str(buf []byte) (string, []byte, error) {
+	buf = buf[1:]
+
+	n, err := strconv.Atoi(string(buf[0]))
+	if err != nil {
+		return "", nil, err
+	}
+
+	return string(buf[3 : n+3]), buf[n+5:], nil
+}
+
+func encodeString(s string) []byte {
+	return []byte(fmt.Sprintf("+%s\r\n", s))
 }
